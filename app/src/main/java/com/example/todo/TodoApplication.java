d0db5555ac2e75993bd8c8e5f9d6b6df3db0b549
windblow1024 <160717168+windblow1024@users.noreply.github.com>
2026-06-13 21:08:27 +0800

    fix: remove PreferenceManager dependency

--- app/src/main/java/com/example/todo/TodoApplication.java
+++ app/src/main/java/com/example/todo/TodoApplication.java
@@ -1,10 +1,6 @@
 package com.example.todo;
 
 import android.app.Application;
-import android.content.SharedPreferences;
-
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.preference.PreferenceManager;
 
 /**
  * Application 类，提供全局数据库实例和应用主题
