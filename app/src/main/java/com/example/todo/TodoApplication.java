0af9cca51991ea09b6cade0f4faee7f4fff33705
windblow1024 <160717168+windblow1024@users.noreply.github.com>
2026-06-13 22:04:43 +0800

    v3.0: Room, matrix, categories, calendar fix, dashboard, attachments

--- app/src/main/java/com/example/todo/TodoApplication.java
+++ app/src/main/java/com/example/todo/TodoApplication.java
@@ -2,26 +2,28 @@ package com.example.todo;
 
 import android.app.Application;
 
+import com.example.todo.data.AppDatabase;
+
 /**
- * Application 类，提供全局数据库实例和应用主题
+ * Application 类
  */
 public class TodoApplication extends Application {
 
     private static TodoApplication instance;
-    private DatabaseHelper databaseHelper;
+    private AppDatabase database;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
-        databaseHelper = new DatabaseHelper(this);
+        database = AppDatabase.getInstance(this);
     }
 
     public static synchronized TodoApplication getInstance() {
         return instance;
     }
 
-    public DatabaseHelper getDatabaseHelper() {
-        return databaseHelper;
+    public AppDatabase getDatabase() {
+        return database;
     }
 }
