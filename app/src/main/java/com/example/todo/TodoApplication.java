a66dd2503e1bd833df5b8311886bf2f64440c77b
windblow1024 <160717168+windblow1024@users.noreply.github.com>
2026-06-13 19:41:01 +0800

    Add app/src/main/java/com/example/todo/TodoApplication.java

---
+++ app/src/main/java/com/example/todo/TodoApplication.java
@@ -0,0 +1,27 @@
+package com.example.todo;
+
+import android.app.Application;
+
+/**
+ * Application 类，提供全局数据库实例
+ */
+public class TodoApplication extends Application {
+
+    private static TodoApplication instance;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        databaseHelper = new DatabaseHelper(this);
+    }
+
+    public static synchronized TodoApplication getInstance() {
+        return instance;
+    }
+
+    public DatabaseHelper getDatabaseHelper() {
+        return databaseHelper;
+    }
+}
