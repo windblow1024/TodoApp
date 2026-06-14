e6bcfe6ac17d50dcb0857ae044a5bc1b75ff38a9
windblow1024 <160717168+windblow1024@users.noreply.github.com>
2026-06-14 14:13:11 +0800

    fix: DatePickerDialog show() 编译错误 - 分离创建和显示

--- app/src/main/java/com/example/todo/TaskDetailActivity.java
+++ app/src/main/java/com/example/todo/TaskDetailActivity.java
@@ -288,10 +288,11 @@ public class TaskDetailActivity extends AppCompatActivity {
                 selectedDueDate = cal.getTimeInMillis();
                 dueDateText.setText(dateTimeFormat.format(selectedDueDate));
             }, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show();
-        }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show();
+        }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
 
         // 设置最小可选日期
         datePicker.getDatePicker().setMinDate(minDate);
+        datePicker.show();
     }
 
     private void handleAttachment(Uri uri) {
